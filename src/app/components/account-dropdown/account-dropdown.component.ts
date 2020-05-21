006058e3caac6e46808e0d61a4807e2be3686355
Duha-H <duha.h.153@gmail.com>
2020-05-21 11:08:04 -0400

    Added dropdown transition

--- src/app/components/account-dropdown/account-dropdown.component.ts
+++ src/app/components/account-dropdown/account-dropdown.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ElementRef, ViewChild } from '@angular/core';
 import { DropdownItem } from 'src/app/views/app-wrapper/app-wrapper.component';
-import { UserStoreService } from 'src/app/models/user-store.service';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -8,15 +7,27 @@ import { UserStoreService } from 'src/app/models/user-store.service';
   templateUrl: './account-dropdown.component.html',
   styleUrls: ['./account-dropdown.component.css']
 })
-export class AccountDropdownComponent implements OnInit {
+export class AccountDropdownComponent implements OnInit, OnChanges {
 
   @Input() dropdownItems: DropdownItem[];
+  @Input() displayDropdown: boolean;
+  @ViewChild("dropdownContainer") dropdownRef: ElementRef;
 
-  constructor(private userStore: UserStoreService) { }
+  constructor() { }
 
   ngOnInit(): void {
     if (!this.dropdownItems) {
       throw new TypeError('AccountDropdown: No dropdown items specified.');
+    }
+  }
+
+  ngOnChanges() {
+    if (this.dropdownRef) {
+      if (this.displayDropdown) {
+        this.dropdownRef.nativeElement.className = 'dropdown-container visible';
+      } else {
+        this.dropdownRef.nativeElement.className = 'dropdown-container hidden';
+      }
     }
   }
 
