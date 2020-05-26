31eb95ce3e9eb22d78460ee882eb5ba617d7782d
Duha-H <duha.h.153@gmail.com>
2020-05-26 12:36:37 -0400

    Minor styling updates + better email sign up validation

--- src/app/components/text-field/text-field.component.ts
+++ src/app/components/text-field/text-field.component.ts
@@ -1,34 +1,41 @@
-import { Component, OnInit, Input, Output, DoCheck, EventEmitter, OnChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+} from "@angular/core";
 
 @Component({
-  selector: 'text-field',
-  template: '\
+  selector: "text-field",
+  template:
+    '\
   <form>\
     <div class="input-container">\
+      <p *ngIf="type == \'email\' && field.invalid" class="warning-text">{{label}} is not valid</p>\
       <div>\
         <input type="{{currType}}" [(ngModel)]="text" [value]="text" (input)="onInput()" \
-          name="inputField" #field="ngModel" placeholder="{{label}}" class="input"/>\
+          name="inputField" #field="ngModel" placeholder="{{label}}" class="input" email="{{type == \'email\'}}"/>\
         <mat-icon *ngIf="type == \'password\'" (click)="togglePasswordVisibility()" >{{visibilityIconName}}</mat-icon>\
       </div>\
       <label *ngIf="displayLabel" for="inputField" >{{fieldLabel}}</label>\
     </div>\
   </form>',
-  styleUrls: ['./text-field.component.css']
+  styleUrls: ["./text-field.component.css"],
 })
 export class TextFieldComponent implements OnInit {
-
-  @Input() label = '';
-  @Input() type = 'text';
+  @Input() label = "";
+  @Input() type = "text";
   @Input() required = false;
   @Input() displayLabel = true;
   @Output() inputChange = new EventEmitter();
   text: string;
   fieldEmpty = true;
   fieldLabel = this.label;
-  visibilityIconName = 'visibility_off';
+  visibilityIconName = "visibility_off";
   currType = this.type;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.currType = this.type;
@@ -36,21 +43,20 @@ export class TextFieldComponent implements OnInit {
 
   onInput() {
     this.inputChange.emit(this.text);
-    if (this.text === '') {
-      this.fieldLabel = '\n';
+    if (this.text === "") {
+      this.fieldLabel = "\n";
     } else {
       this.fieldLabel = this.label;
     }
   }
 
   togglePasswordVisibility() {
-    if (this.visibilityIconName === 'visibility') {
-      this.visibilityIconName = 'visibility_off';
-      this.currType = 'password';
+    if (this.visibilityIconName === "visibility") {
+      this.visibilityIconName = "visibility_off";
+      this.currType = "password";
     } else {
-      this.visibilityIconName = 'visibility';
-      this.currType = 'text';
+      this.visibilityIconName = "visibility";
+      this.currType = "text";
     }
   }
-
 }
