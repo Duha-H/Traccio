d6cdf703670b2ee325d7bb080a1dfb8ebecc0bad
Duha-H <duha.h.153@gmail.com>
2020-07-15 23:14:25 -0400

    Handled profile-settings view responsiveness

--- src/app/views/settings/profile-settings.component.ts
+++ src/app/views/settings/profile-settings.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
 import { STATE_ATTRIBS } from 'src/app/controllers/app-state-store.service';
 import { TextFieldComponent } from 'src/app/components/text-field/text-field.component';
 import { Response } from 'src/app/utils/response';
 import { User } from 'src/app/models/user';
 import { UserStoreService } from 'src/app/models/user-store.service';
 import { DEFAULT_PROFILE_UPDATE_CHECK } from './constants';
+import { ResizeService } from 'src/app/controllers/resize.service';
 
 @Component({
   selector: 'settings-profile',
   templateUrl: './profile-settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
+  // tslint:disable-next-line: no-host-metadata-property
+  host: {
+    "(document:click)": "onViewClick($event)",
+  },
 })
 export class ProfileSettingsComponent implements OnInit {
 
@@ -25,12 +30,13 @@ export class ProfileSettingsComponent implements OnInit {
     confirmPassword: '',
   };
   updateList: {[key: string]: string } = {};
+  visibleTooltip: ElementRef = undefined;
 
   @Output() updates: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() updatePassword: EventEmitter<object> = new EventEmitter<object>();
   @Output() showAlert: EventEmitter<Response> = new EventEmitter<Response>();
 
-  constructor(private userStore: UserStoreService) { }
+  constructor(private userStore: UserStoreService, public rs: ResizeService) { }
 
   ngOnInit() {
     this.userStore.user.subscribe(user => {
@@ -76,6 +82,36 @@ export class ProfileSettingsComponent implements OnInit {
 
   toggleChangePassword() {
     this.changePassword = !this.changePassword;
+  }
+
+  displayTooltip(id: string, event: MouseEvent) {
+    if (this.visibleTooltip) {
+      this.visibleTooltip.nativeElement.className = 'info'; // hide already-visible tooltip
+    }
+    this.visibleTooltip = new ElementRef(document.getElementById(id));
+    this.visibleTooltip.nativeElement.style.top = `${event.y + 20}px`;
+    this.visibleTooltip.nativeElement.style.left = `${event.x + 20}px`;
+    this.visibleTooltip.nativeElement.className += ' visible';
+    setTimeout(() => {
+      this.hideTooltip();
+    }, 8000);
+    event.preventDefault();
+  }
+
+  hideTooltip() {
+    if (this.visibleTooltip) {
+      this.visibleTooltip.nativeElement.className = 'info';
+      this.visibleTooltip = undefined;
+    }
+  }
+
+  onViewClick(event: MouseEvent) {
+    let tooltipVisible = false;
+    if (this.visibleTooltip && !this.visibleTooltip.nativeElement.contains(event.target)) {
+      console.log('hiding ittt');
+      tooltipVisible = true;
+      // this.hideTooltip();
+    }
   }
 
   async verifyEmail(code: string) {
