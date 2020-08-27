d7aaae6d3cf3fd62d67dd17edec5730f98f80203
Duha-H <duha.h.153@gmail.com>
2020-08-26 21:28:31 -0400

    Additional About view styling

--- src/app/views/landing/about.component.ts
+++ src/app/views/landing/about.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import { ResizeService } from 'src/app/controllers/resize.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css', './landing.component.css']
+  styleUrls: ['./about.component.css', './landing.component.css'],
 })
 export class AboutComponent implements OnInit {
 
   now = new Date();
   timeOfDay = 'morning';
   displayMobileNav = false;
+  @ViewChild('mobileNav') mobileNav: ElementRef;
 
-  constructor(public resizeService: ResizeService) { }
+  constructor(public resizeService: ResizeService, private router: Router) { }
 
   ngOnInit() {
     const hour = this.now.getHours();
@@ -26,13 +28,21 @@ export class AboutComponent implements OnInit {
   }
 
   scroll(id: string) {
-    const element = document.querySelector(id);
-    console.log(element);
-    element.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+    const element = document.querySelector(`#${id}`);
+    element.scrollIntoView({ behavior: 'smooth', block: 'nearest', });
+    // this.router.navigate(['about'], { fragment: id }); // fragment scrolling is messy
     element.classList.add('pulse');
     setTimeout(() => {
       element.classList.remove('pulse');
     }, 500);
     this.displayMobileNav = false;
+  }
+
+  onWrapperClick(event: Event) {
+    // If a click is registered outside of the nav toggle button
+    // hide mobileNav
+    if (this.displayMobileNav && !this.mobileNav.nativeElement.contains(event.target)) {
+      this.displayMobileNav = false;
+    }
   }
 }
